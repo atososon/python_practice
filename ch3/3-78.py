51808e7d1016dda9ba0d24289d9e936230122f2f
atososon <atososon165@gmail.com>
2020-04-18 20:47:48 +0900

    配列の練習

---
+++ ch3/3-78.py
@@ -0,0 +1,7 @@
+surprise = ["Groucho", "Chico", "Harpo"]
+surprise[2] = surprise[2].lower()
+print(surprise)
+surprise.reverse()
+print(surprise)
+surprise[0] = surprise[0].capitalize()
+print(surprise)
