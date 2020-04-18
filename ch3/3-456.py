89ddaf3bf858193b55513d0e9dcfbb33b0a804d4
atososon <atososon165@gmail.com>
2020-04-18 20:29:09 +0900

    配列の中身を変更する

---
+++ ch3/3-456.py
@@ -0,0 +1,7 @@
+things = ["mozzarella", "cinderella", "salmonella"]
+things[1] = things[1].capitalize()
+print(things)
+things[0] = things[0].upper()
+print(things)
+things[2] = "撲滅できればノーベル賞ものだろう"
+print(things)
