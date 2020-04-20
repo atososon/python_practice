08fd02143da7de8f8b69e09aa8bd0596c1d972ef
atososon <atososon165@gmail.com>
2020-04-20 20:37:11 +0900

    for文の練習

---
+++ ch4/4-1.py
@@ -0,0 +1,18 @@
+guess_me = 7
+if guess_me < 7 :
+    print("too low")
+elif guess_me > 7 :
+    print("too high")
+else :
+    print("just right")
+
+start = 1
+while True:
+    if(start < guess_me):
+        print("too low")
+    elif(start == guess_me):
+        print("found it!")
+    else:
+        print("oops")
+        break
+    start+=1
