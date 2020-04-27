cb2d92f58aabc1a21d5e700652698581a5002f41
atososon <atososon165@gmail.com>
2020-04-27 21:36:17 +0900

    デコレータの練習

---
+++ ch4/4-10.py
@@ -0,0 +1,13 @@
+def test(func):
+    def new_func(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return new_func
+ 
+@test
+def greeting():
+    print("Greeting, Earthling")
+
+greeting()
