0cd26cc0ea1670fbe0550a8492fd8e567463fa68
atososon <atososon165@gmail.com>
2020-05-11 21:31:52 +0900

    オブジェクトとクラスの練習

---
+++ ch6/6-4.py
@@ -0,0 +1,7 @@
+class Element:
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+
+hydrogen = Element('Hydrogen', 'H', 1)
